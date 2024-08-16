221626c72d02f0c1264e760c6798f2fbfb141141
jay <jaywest650@gmail.com>
2024-08-16 13:37:39 +0000

    get learning category fix

--- api-v2-junior.php
+++ api-v2-junior.php
@@ -196,7 +196,7 @@ if (isset($_POST['access_key']) && isset($_POST['get_categories_by_language'])) 
         if ($type == 2 || $type == '2') {
             $sql = "SELECT *, (SELECT count(id) FROM tbl_junior_learning where tbl_junior_learning.category=c.id ) as no_of,
 			(select `language` from `languages` l where l.id = c.language_id ) as language
-			FROM `category` c where `language_id` = " . $language_id . " AND c.status='1' AND c.type=" . $type . " ORDER By CAST(c.row_order as unsigned) ASC";
+			FROM `junior_category` c where `language_id` = " . $language_id . " AND c.status='1' AND c.type=" . $type . " ORDER By CAST(c.row_order as unsigned) ASC";
         }
         if ($type == 3 || $type == '3') {
             $sql = "SELECT *, (SELECT count(id) FROM tbl_maths_question where tbl_maths_question.category=c.id ) as no_of_que,
@@ -289,7 +289,7 @@ if (isset($_POST['access_key']) && isset($_POST['get_categories'])) {
           
         }
         if ($type == 2 || $type == '2') {
-            $sql = "SELECT *, (SELECT count(id) FROM tbl_learning where tbl_learning.category=c.id ) as no_of FROM `category` c WHERE c.id = $id AND c.status='1'AND c.type=" . $type . " ORDER BY CAST(c.row_order as unsigned) ASC";
+            $sql = "SELECT *, (SELECT count(id) FROM tbl_junior_learning where tbl_junior_learning.category=c.id ) as no_of FROM `junior_category` c WHERE c.id = $id AND c.status='1'AND c.type=" . $type . " ORDER BY CAST(c.row_order as unsigned) ASC";
         }
         if ($type == 3 || $type == '3') {
             $sql = "SELECT *, (SELECT count(id) FROM tbl_maths_question where tbl_maths_question.category=c.id ) as no_of_que, (SELECT @no_of_subcategories := count(`id`) from subcategory s WHERE s.maincat_id = c.id and s.status = 1 ) as no_of FROM `category` c WHERE c.id = $id AND c.status='1' AND c.type=" . $type . " ORDER BY CAST(c.row_order as unsigned) ASC";
